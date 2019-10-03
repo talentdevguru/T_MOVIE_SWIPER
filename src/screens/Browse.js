067ae8a9b989e79568814053ae981c851464d7f5
TalentDev Guru <talent.dev.guru@gmail.com>
2019-10-02 17:38:41 -0700

    no message

--- src/screens/Browse.js
+++ src/screens/Browse.js
@@ -123,7 +123,7 @@ class Browse extends React.Component {
     renderBrowseSections() {
         const { sectionsMovies } = this.state;
         const keyExtractor = section => section.title;
-        //def
+        //defi
         return (
             <FlatList 
                 data={BROWSE_SECTIONS}
