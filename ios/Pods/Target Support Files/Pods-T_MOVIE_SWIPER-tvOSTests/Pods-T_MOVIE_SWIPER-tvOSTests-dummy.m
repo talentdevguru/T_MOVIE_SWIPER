6f1df8e8fef9da1bc987ceddd7b9f135295f506b
TalentDev Guru <talent.dev.guru@gmail.com>
2019-10-15 22:48:05 +0900

    Initial Projet's  Structure

---
+++ ios/Pods/Target Support Files/Pods-T_MOVIE_SWIPER-tvOSTests/Pods-T_MOVIE_SWIPER-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_T_MOVIE_SWIPER_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_T_MOVIE_SWIPER_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-T_MOVIE_SWIPERTests/Pods-T_MOVIE_SWIPERTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_T_MOVIE_SWIPERTests : NSObject
+@end
+@implementation PodsDummy_Pods_T_MOVIE_SWIPERTests
+@end
