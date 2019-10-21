c442d5eb84d6ca57053b376c5a6aa91fd59c2f8a
TalentDev Guru <talent.dev.guru@gmail.com>
2019-10-21 22:47:17 +0900

    no message

--- src/Routes.js
+++ src/Routes.js
@@ -1,42 +1,44 @@
 import React from 'react';
 import {
     createAppContainer,
-    createStackNavigator,
     createSwitchNavigator,
     createBottomTabNavigator,
     StackActions,
 } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import Splash from './screens/Splash';
-import AuthWelcome from './screens/Auth/AuthWelcome';
-import AuthLogin from './screens/Auth/AuthLogin';
-import Header from './components/Header';
+// import AuthWelcome from './screens/Auth/AuthWelcome';
+// import AuthLogin from './screens/Auth/AuthLogin';
+// import Header from './components/Header';
 
 import RouteNames from './RouteNames';
 
-const defaultHeaderObject = {
-    header: props => <Header scene={props.scene} />
-};
+// const defaultHeaderObject = {
+//     header: props => <Header scene={props.scene} />
+// };
 
-const createDefaultStackNavigator = (screenObject, customOptions) =>
-    createStackNavigator(screenObject, {
-        defaultNavigationOptions: { ...defaultHeaderObject },
-        cardStyle: {
-            backgroundColor: '#000'
-        },
-        headerMode: 'screen',
-        transitionConfig: () => fromRightWitFade(),
-        ...customOptions
-    });
+// const createDefaultStackNavigator = (screenObject, customOptions) =>
+//     createStackNavigator(screenObject, {
+//         defaultNavigationOptions: { ...defaultHeaderObject },
+//         cardStyle: {
+//             backgroundColor: '#000'
+//         },
+//         headerMode: 'screen',
+//         transitionConfig: () => fromRightWitFade(),
+//         ...customOptions
+//     });
 
-const AuthStack = createDefaultStackNavigator({
-    [RouteNames.AuthWelcome]: { screen: AuthWelcome },
-    [RouteNames.AuthLogin]: { screen: AuthLogin }
-})
+// const AuthStack = createDefaultStackNavigator({
+//     [RouteNames.AuthWelcome]: { screen: AuthWelcome },
+//     [RouteNames.AuthLogin]: { screen: AuthLogin }
+// })
 
 export const RootStack = createAppContainer(
-    createSwitchNavigator({
-        [RouteNames.Splash]: { screen: Splash },
-        [RouteNames.AuthStack]: { screen: AuthStack }
-    })
+    
+    {[RouteNames.Splash]: { screen: Splash }}
+    // createSwitchNavigator({
+        
+        // [RouteNames.AuthStack]: { screen: AuthStack }
+    // })
 );
