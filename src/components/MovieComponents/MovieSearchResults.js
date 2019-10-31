6be48ea04f6a04031c3640790113094a1ce972b5
TalentDev Guru <talent.dev.guru@gmail.com>
2019-11-01 04:06:02 +0900

    no message

--- src/components/MovieComponents/MovieSearchResults.js
+++ src/components/MovieComponents/MovieSearchResults.js
@@ -3,5 +3,5 @@ import PropTypes from 'prop-types';
 import { View, StyleSheet } from 'react-native';
 import InfoAbsoluteBlock from '../InfoAbsoluteBlock';
 import MovieFetchList from './MovieFetchLList';
-import { getEmptySearchIcon, getInitialSearchIcon } from '../../../utils/icons';
-import Theme from '../../../Theme';
+import { getEmptySearchIcon, getInitialSearchIcon } from '../../utils/icons';
+import Theme from '../../Theme';
