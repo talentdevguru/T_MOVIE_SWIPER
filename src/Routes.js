d2a62d6b6af004f0e651d50e37cf4d09add9f863
TalentDev Guru <talent.dev.guru@gmail.com>
2019-11-04 09:51:18 +0900

    no message

--- src/Routes.js
+++ src/Routes.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {
     createAppContainer,
     createSwitchNavigator,
-    createBottomTabNavigator,
     StackActions,
 } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import Splash from './screens/Splash';
