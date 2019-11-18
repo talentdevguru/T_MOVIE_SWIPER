c21771bdcb625c8fa9ddba670509e0624a39c476
TalentDev Guru <talent.dev.guru@gmail.com>
2019-11-18 08:52:37 -0800

    no message

--- src/screens/Auth/AuthWelcome.js
+++ src/screens/Auth/AuthWelcome.js
@@ -45,7 +45,7 @@ class AuthWelcome extends React.Component {
   showToast = message => this.toast.show(message, 2000);
 
   render() {
-      
+    //define const isGuestSessionCreating  
     const { isGuestSessionCreating } = this.props;
     
     return (
@@ -62,6 +62,7 @@ class AuthWelcome extends React.Component {
             <AppText style={styles.welcomeText} type="title1">
               Welcome to MovieSwiper
             </AppText>
+    
             <AppText style={styles.welcomeCaption} type="titleCaption">
               Powered by The Movie Database
             </AppText>
