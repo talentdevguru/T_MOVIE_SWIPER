2f52e048b8693cc6c482dc8fe06f44b4bccfc167
TalentDev Guru <talent.dev.guru@gmail.com>
2019-12-08 11:32:02 -0800

    no message

--- src/screens/Browse.js
+++ src/screens/Browse.js
@@ -120,7 +120,7 @@ class Browse extends React.Component {
                 renderItem={this.renderMoviesScrollSection}
                 showsVerticalScrollIndicator={false}
             />
-
+sd
         );
     }
 
